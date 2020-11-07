b6f1e4df0c7e19b75f1ec90e348a914c82edce90
nokl <naoki.tkb@gmail.com>
2020-11-07 18:46:31 +0900

    update: specify used minor core-js version

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ const webpackConfig = {
                                     '@babel/preset-env',
                                     {
                                         useBuiltIns: 'usage',
-                                        corejs: 3,
+                                        corejs: '3.7',
                                     },
                                 ],
                             ],
