09156b715efff7ae8b14c0b400a15bddebb2fe6b
naoki <naoki.tkb@gmail.com>
2021-09-28 23:15:43 +0900

    chore

--- webpack.config.js
+++ webpack.config.js
@@ -80,7 +80,7 @@ const webpackConfig = {
                                     '@babel/preset-env',
                                     {
                                         useBuiltIns: 'usage',
-                                        corejs: '3.17',
+                                        corejs: '3.18',
                                     },
                                 ],
                             ],
