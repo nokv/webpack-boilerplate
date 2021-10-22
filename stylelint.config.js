b7c5076b3d780606f40a4616b8b1bc63e726dc0d
naoki <naoki.tkb@gmail.com>
2021-10-22 20:39:15 +0900

    update: stylelint v14

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,12 +1,17 @@
 /** @type {import('@types/stylelint').Configuration} */
 const config = {
-    extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-standard-scss',
+        'stylelint-config-prettier',
+    ],
     plugins: ['stylelint-scss'],
     // https://stylelint.io/user-guide/configuration
     rules: {
         'at-rule-no-unknown': null,
         'scss/at-rule-no-unknown': true,
         'no-invalid-position-at-import-rule': null,
+        'media-feature-name-no-vendor-prefix': null,
     },
 };
 module.exports = config;
