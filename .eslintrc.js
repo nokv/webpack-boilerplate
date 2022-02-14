fed3b68af1d989e5b64de8239f570a4d19e36884
naoki <naoki.tkb@gmail.com>
2022-02-14 21:13:13 +0900

    update: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,32 @@
-/** @type {import('@typescript-eslint/experimental-utils').TSESLint.Linter.Config} */
+/** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 const config = {
     root: true,
     env: {
         browser: true,
         node: true,
+        es2021: true,
     },
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
         'prettier',
     ],
-    plugins: ['@typescript-eslint'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 2020,
+        project: ['tsconfig.json'],
+        ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    // add your custom rules here
+    plugins: ['@typescript-eslint', 'import'],
     rules: {
         quotes: ['error', 'single'],
         'no-unused-vars': 'warn',
+        '@typescript-eslint/no-var-requires': 'warn',
+        '@typescript-eslint/ban-ts-comment': 'warn',
+        'import/no-unresolved': 'off',
     },
     overrides: [
         {
